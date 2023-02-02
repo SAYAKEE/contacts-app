62987c8c1eb5c08a0759defd23872fbef045a58d
HAN ZAW HTET <arjinomoto268@gmail.com>
2023-02-02 21:31:58 +0630

    1 - index.php created without php codes and is not connected to database.

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacts</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <header class="text-center container">
+        <h1>CONTACTS</h1>
+        <hr>
+        <button class="btn btn-info w-100"><a href="add.php">+ ADD NEW CONTACT</a></button>
+    </header>
+    <main class="container text-center">
+        <table class="table">
+            <thead>
+                <th>Name</th>
+                <th>Phone  Number</th>
+                <th>Actions</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>KO ARR LUU</td>
+                    <td>+349569238</td>
+                    <td>
+                        <a href="edit">Edit</a>
+                        <a href="delete">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>KO ARR LUU</td>
+                    <td>+349569238</td>
+                    <td>
+                        <a href="edit">Edit</a>
+                        <a href="delete">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>KO ARR LUU</td>
+                    <td>+349569238</td>
+                    <td>
+                        <a href="edit">Edit</a>
+                        <a href="delete">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>KO ARR LUU</td>
+                    <td>+349569238</td>
+                    <td>
+                        <a href="edit">Edit</a>
+                        <a href="delete">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
